04ae82b776fc0a921a39b54f319022c8b74e07c0
Indy Adira Khalfani <indyadira@gmail.com>
2023-05-04 22:02:56 +0700

    first commit

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+    $connect = mysqli_connect("localhost", "root", "", "uts_pemrograman_web");
+    // if ($connect)
+    // {
+    //     echo "koneksi berhasil";
+    // }
+?>
---
+++ halaman_daftar.php
@@ -0,0 +1,4 @@
+<?php
+    print("<label>username   :</label>
+    ");
+?>
