60bcbdbc3b5379b57809597b8b3eafffbfd5525e
Indy Adira Khalfani <indyadira@gmail.com>
2023-05-06 00:02:09 +0700

    06/05/2023 - 00:02

--- halaman_daftar.php
+++ halaman_daftar.php
@@ -11,29 +11,26 @@
     </head>
     <body>
         <div>
-            <table cellspacing="10">
+            <table cellspacing="10" >
                 <tr>
                     <td><label for="username">username</label></td>
                     <td>:</td>
-                    <td><input type="text" id="username"></td>
-                </tr>
-                <tr>
-                    <td colspan="3"></td>
+                    <td colspan="2"><input type="text" id="username"></td>
                 </tr>
                 <tr>
                     <td><label for="password">password</td>
                     <td>:</td>
-                    <td><input type="password" id="password"></td>
+                    <td colspan="2"><input type="password" id="password"></td>
                 </tr>
                 <tr>
                     <td><label for="quotes">quotes</label></td>
                     <td>:</td>
-                    <td><textarea name="quotes" id="quotes"></textarea></td>
+                    <td><textarea name="quotes" id="quotes" rowspan="2"></textarea></td>
                 </tr>
                 <tr>
                     <td><label for="favorite">karakter favorite</label></td>
                     <td>:</td>
-                    <td>
+                    <td colspan="2">
                         <select name="favorite" id="favorite">
                             <option value="0">Pilih karakter favorite</option>
                             <?php
@@ -49,6 +46,12 @@
                             ?>
                         </select>
                     </td>
+                </tr>
+                <tr>
+                    <td><label for="jenis_kelamin">jenis kelamin</label></td>
+                    <td>:</td>
+                    <td><input type="radio" name="jenis_kelamin" id="jenis_kelamin">laki - laki</td>
+                    <td><input type="radio" name="jenis_kelamin" id="jenis_kelamin">perempuan</td>
                 </tr>
             </table>
         </div>
