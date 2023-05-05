753876fb8a1217c51abb2137698d871fe9a2ee82
Indy Adira Khalfani <indyadira@gmail.com>
2023-05-05 08:52:20 +0700

    05/05/2023 - 08.52

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+    <link rel="stylesheet" type="text/css" href="/uts_pemrograman_web/css/index.css">
+    <head>
+        <title>
+            pemrograman web &minus; tugas UTS
+        </title>
+        <link rel="icon" href="image/mahiru.jpg" type = "image/x-icon">
+    </head>
+    <body class="background"> 
+        <div class="logo_The_Angel_Next_Door_Spoils_Me_Rotten_Wiki">
+            <a href="https://otonari-no-tenshi.fandom.com/wiki/Otonari_no_Tenshi-sama_Wiki">
+                <img src="image/halaman_pertama/logo_The Angel Next Door Spoils Me Rotten.png" alt="image" style="width: 340px;">
+            </a>
+        </div>
+
+        <div class="gambar">
+            <img src="image/halaman_pertama/profile.jpg" alt="image" style="width: 200px; height: 200px; border-radius: 60%;border-style: solid; border-width: 10px; border-color: white; border-radius: 60%;">
+        </div>
+
+        <div class="nama">
+            <h1> Login or Register</h1>
+        </div>
+
+        <a href="/uts_pemrograman_web/halaman_login.php" >
+            <div class="tombol_404_not_found">
+                <p>Login account</p>
+            </div>
+        </a>
+        
+        <a href="/uts_pemrograman_web/halaman_daftar.php">
+            <div class="tombol_halaman_dalam_tahap_pengembangan">
+                <p> register account</p>
+            </div>
+        </a>
+    </body>
+</html>
+<!-- untuk penggunaan karakter khusus character entity bisa dilihat di https://oinam.github.io/entities/-->
